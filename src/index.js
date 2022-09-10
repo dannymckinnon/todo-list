3765fe7bae94bf48e1f18c2fa0a3008d631966a9
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-09-09 22:44:46 -0700

    imported images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { setupEventListeners, displayTasks, displayProjects } from './modules/domFunctionality.js';
 import { todoArray, projectArray, localStorageToArray, projectLocalToArray } from './modules/appLogic.js';
 import './styles/style.css';
+import './images';
 
 setupEventListeners();
 document.querySelector('.all').style.backgroundColor = 'rgb(231, 231, 231)';
 
 localStorageToArray();
 projectLocalToArray();
-
 
 displayTasks(todoArray);
 displayProjects(projectArray);
