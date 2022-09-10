8718a4ff9614ad829ce5afc44134a38734cdb4c6
Danny McKinnon <dannymckinnon.dm@gmail.com>
2022-09-09 23:34:49 -0700

    imported images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { setupEventListeners, displayTasks, displayProjects } from './modules/domFunctionality.js';
 import { todoArray, projectArray, localStorageToArray, projectLocalToArray } from './modules/appLogic.js';
 import './styles/style.css';
-import './images';
+import add from './images/add.svg';
+import check from './images/check.svg';
+import close from './images/close.svg';
+import edit from './images/edit.svg';
+import inbox from './images/inbox.svg';
+import projects from './images/projects.svg';
+import today from './images/today.svg';
+import upcoming from './images/upcoming.svg';
 
 setupEventListeners();
 document.querySelector('.all').style.backgroundColor = 'rgb(231, 231, 231)';
